b0080c3f767df9da5b086b9da482bcb693f16ccf
Godot115 <godot.is.me@gmail.com>
2023-02-24 17:58:13 +0800

    Write the model into the code.

--- comparison.py
+++ comparison.py
@@ -20,6 +20,7 @@ from numpy import array
 
 from models.custom_model import CustomModel
 from models.doe_problem import DoeProblem
+from models.logistic_model import LogisticModel
 from models.model2 import Model2
 from models.model3_negative import Model3Negative
 from models.model3_positive import *
@@ -57,7 +58,7 @@ def write_log(algorithm, theta, seed, best_fitness, best_history):
 if __name__ == "__main__":
 
     paras = {
-        "epoch": 5000,
+        "epoch": 10000,
         "pop_size": 100,
     }
     models = []
@@ -80,13 +81,16 @@ if __name__ == "__main__":
                   "t3": [0.018, 0.953, 0.755, 0.409, 0.083, 0.632, 0.289]}
         for theta in thetas:
             print(theta)
-            model = CustomModel("1/(1+e**(-1*(t_0+t_1 *x_1+t_2*x_2+t_3*x_3 + t_4*x_4 + t_5*x_5 + t_6*x_6)))",
-                                ["x_1", "x_2", "x_3", "x_4", "x_5", "x_6"],
-                                ["t_0", "t_1", "t_2", "t_3", "t_4", "t_5", "t_6"],
-                                thetas[theta])
-
+            model = LogisticModel(thetas[theta])
             problem_doe = DoeProblem(model, "D-optimal", grid_size=1001, lob=[0], hib=[1], minmax="max", dimension=6,
                                      num_of_sup=8, log_to=None)
+            # model = CustomModel("1/(1+e**(-1*(t_0+t_1 *x_1+t_2*x_2+t_3*x_3 + t_4*x_4 + t_5*x_5 + t_6*x_6)))",
+            #                     ["x_1", "x_2", "x_3", "x_4", "x_5", "x_6"],
+            #                     ["t_0", "t_1", "t_2", "t_3", "t_4", "t_5", "t_6"],
+            #                     thetas[theta])
+
+            # problem_doe = DoeProblem(model, "D-optimal", grid_size=1001, lob=[0], hib=[1], minmax="max", dimension=6,
+            #                          num_of_sup=8, log_to=None)
             starting_positions = create_starting_positions(problem_doe, paras["pop_size"])
             best_fit = float('-inf')
             for pos in starting_positions:
@@ -100,4 +104,4 @@ if __name__ == "__main__":
                 for epoch_best in model.history.list_global_best:
                     best.append(epoch_best[1][0])
                 write_log(model.get_name(), theta, seed, best_fitness, best)
-                print(time.time(), model.get_name())
+                print(time.time(), model.get_name(),best_fitness)
